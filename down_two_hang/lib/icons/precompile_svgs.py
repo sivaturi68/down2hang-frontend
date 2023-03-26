8fe632d578c30e2438793036e173a01ded7dcaf3
Nikhil Ivaturi <nikhilivaturi25@gmail.com>
2023-03-26 18:42:53 -0400

    added script to precompile svg files

---
+++ down_two_hang/lib/icons/precompile_svgs.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+
+#!!! IMPORTANT: run this from the down_two_hang dir
+
+files = os.listdir('./lib/icons/')
+files = list(filter(lambda x: x.endswith('.svg'), files))
+
+if len(files) == 0:
+    print('no files found, exiting')
+    exit(1)
+
+for fi in files:
+    command = f'dart run vector_graphics_compiler -i ./lib/icons/{fi} -o ./lib/icons/{fi}.vec'
+    command = command.split()
+    print(f'command: {command}')
+    p = subprocess.Popen(command, shell=True)
+    p.wait()
