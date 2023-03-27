55ec02b2625911b967fafd94e51c82574cbe6fdf
Nikhil Ivaturi <nikhilivaturi25@gmail.com>
2023-03-26 20:18:07 -0400

    forgot to mark something const

--- down_two_hang/lib/login.dart
+++ down_two_hang/lib/login.dart
@@ -67,7 +67,7 @@ class LoginPage extends StatelessWidget {
               const SizedBox(height: 10),
 
               /* forgot login info */
-              Center(
+              const Center(
                 child: Text(
                   'Forgot login info?',
                   style: TextStyle(
